59cd33bbfac421e224a6e639cd6c1d327d3ccb45
Claudio Benghi <claudio.benghi@gmail.com>
2020-04-27 19:06:29 +0200

    Revised boolean testing.

--- geometry3Test/test_Bool.cs
+++ geometry3Test/test_Bool.cs
@@ -54,10 +54,89 @@ namespace geometry3Test
 
         internal static void test_all()
         {
+            test_programmatic();
+            return;
+
             test_union();
             test_subtraction();
             test_subtraction2();
             test_intersection();
+        }
+
+        private static void test_programmatic()
+        {
+            var outer = MakeBox(
+                center: new Vector3d(0, 0, 0),
+                size: new Vector3d(3, 3, 1)
+                );
+            var hole = MakeBox(
+                center: new Vector3d(0, 0, 0),
+                size: new Vector3d(1, 1, 2)
+                );
+            DMesh3 ret = ComputeBoolean(outer, hole, MeshBoolean.boolOperation.Subtraction, true);
+
+            List<DMesh3> cuts = new List<DMesh3>();
+
+            cuts.Add(MakeBox(
+                center: new Vector3d(1.5, 1.5, 0),
+                size: new Vector3d(.5, .5, .5)
+                ));
+
+            cuts.Add(MakeBox(
+                center: new Vector3d(-1.5, -1.5, 0),
+                size: new Vector3d(.5, .5, .5)
+                ));
+            cuts.Add(MakeBox(
+                center: new Vector3d(1.5, -1.5, 0),
+                size: new Vector3d(.5, .5, .5)
+                ));
+            cuts.Add(MakeBox(
+                center: new Vector3d(-1.5, 1.5, 0),
+                size: new Vector3d(.5, .5, .5)
+                ));
+
+            foreach (var anotherHole in cuts)
+            {
+                ret = ComputeBoolean(ret, anotherHole, MeshBoolean.boolOperation.Subtraction, true);
+            }
+
+
+            var outF = TestUtil.WriteTestOutputMesh(ret, "programmatic.obj");
+            // outF = TestUtil.WriteTestOutputMesh(anotherHole, "h.obj");
+        }
+
+        private static DMesh3 ComputeBoolean(DMesh3 outer, DMesh3 hole, MeshBoolean.boolOperation op, bool full = true)
+        {
+            var mBool = new MeshBoolean();
+            mBool.Target = outer;
+            mBool.Tool = hole;
+            mBool.Compute(op);
+            var ret = mBool.Result;
+
+            if (full)
+            {
+                PlanarRemesher p = new PlanarRemesher(ret);
+                p.Remesh();
+
+                MergeCoincidentEdges mrg = new MergeCoincidentEdges(ret);
+                mrg.ApplyIteratively();
+
+                MeshRepairOrientation rep = new MeshRepairOrientation(ret);
+                rep.OrientComponents();
+                rep.SolveGlobalOrientation();
+            }
+            return ret;
+        }
+
+        private static DMesh3 MakeBox(Vector3d center, Vector3d size)
+        {
+            TrivialBox3Generator boxgen = new TrivialBox3Generator();
+            boxgen.WantNormals = false;
+            boxgen.Box = new Box3d(center, size / 2);
+            boxgen.Generate();
+            DMesh3 mesh = new DMesh3(false, false, false, false);
+            boxgen.MakeMesh(mesh);
+            return mesh;
         }
 
         internal static void test_union()
