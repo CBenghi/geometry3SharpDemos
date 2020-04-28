81d79b17106f2410ae9694773a98485cd726c4e8
Claudio Benghi <claudio.benghi@gmail.com>
2020-04-28 23:25:09 +0200

    Added class to old vs project.
    delBinObj helps with compilation issues when changing from one solution
    to another.

---
+++ delBinObj.cmd
@@ -0,0 +1,8 @@
+del g3MeshConvert\bin\*.* /Q
+del g3MeshConvert\obj\*.* /Q
+del geometry3Sharp\bin\*.* /Q
+del geometry3Sharp\obj\*.* /Q
+del geometry3Test\bin\*.* /Q
+del geometry3Test\obj\*.* /Q
+del UI\GeometryProcessing\bin\*.* /Q
+del UI\GeometryProcessing\obj\*.* /Q
